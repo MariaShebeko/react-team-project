2f9a9361a2248ce25d015eca4f59477bf911cc7f
MariaShebeko <marygro89@gmail.com>
2021-12-06 09:07:49 +0200

    Created Price Table Component

---
+++ src/pages/PriceTable/PriceTable.js
@@ -0,0 +1,89 @@
+import Section from '../../components/base/Section';
+import Container from '../../components/base/Container';
+// import Title from '../../components/base/Title';
+import { Card } from './Card';
+import { DefaultButton } from '../../components/Button';
+import CardsContent from '../../data/PriceTableContent.json';
+import styled from 'styled-components';
+
+const PriceTableText = styled.p`
+  margin-bottom: 50px;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.5;
+`;
+const TableList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+`;
+const CardTitle = styled.h3`
+  font-size: 24px;
+  line-height: 1.66;
+  font-weight: bold;
+`;
+const CardDescription = styled.p`
+  font-size: 16px;
+  line-height: 2.5;
+`;
+const PriceWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Price = styled.div`
+  font-size: 76px;
+  font-weight: bold;
+  line-height: 1.3;
+`;
+const Text = styled.p`
+  margin-bottom: 16px;
+  font-size: 16px;
+  line-height: 1.3;
+
+  &:last-child {
+    margin-bottom: 38px;
+  }
+`;
+const ButtonOrder = styled(DefaultButton)`
+  margin-bottom: 8px;
+`;
+
+const PriceTable = () => {
+  return (
+    <>
+      <Section>
+        <Container>
+          {/* add Title */}
+          {/* <Title></Title> */}
+          <PriceTableText>We offer competitive price</PriceTableText>
+          <TableList>
+            {CardsContent.map(card => (
+              <Card cardType={card.cardType} key={card.title}>
+                <CardTitle>{card.title}</CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+                <PriceWrapper>
+                  <Price>
+                    <p>{card.price}</p>
+                  </Price>
+                  {/* style this elements */}
+                  <div>
+                    <p>{card.currency}</p>
+                    <p>{card.duration}</p>
+                  </div>
+                </PriceWrapper>
+                <div>
+                  <Text>{card.operators}</Text>
+                  <Text>{card.notification}</Text>
+                  <Text>{card.typeCite}</Text>
+                </div>
+                <ButtonOrder btnType={card.btnType}>{card.btnText}</ButtonOrder>
+              </Card>
+            ))}
+          </TableList>
+        </Container>
+      </Section>
+    </>
+  );
+};
+
+export default PriceTable;
