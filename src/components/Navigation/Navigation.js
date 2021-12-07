75e139671761783f69a169803f5dc7ead1ab37bd
Bogdan-Kasyanchuk <bogdan.k1990@gmail.com>
2021-12-07 02:42:27 +0200

    add header styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,36 +1,54 @@
-import { Button } from '../Button';
+import styled from 'styled-components';
+import { getStyles } from '../../layout/layouts';
+const styles = getStyles();
+
+const NavWrapper = styled.nav`
+  margin-left: auto;
+`;
+
+const NavList = styled.ul`
+  margin-right: 24px;
+  width: 380px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavItem = styled.li`
+  padding: 5px;
+`;
+
+const NavLink = styled.a`
+font-weight: 400;
+font-size: 16px;
+line-height: 1.75;
+color: ${styles.mainFontColor};
+  }
+
+  :hover,
+  :focus {
+    font-weight: 600;
+  }
+`;
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Product</a>
-        </li>
-        <li>
-          <a href="/">Customers</a>
-        </li>
-        <li>
-          <a href="/">Pricing</a>
-        </li>
-        <li>
-          <a href="/">Resources</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Button btnType="light" type="button">
-            Sign In
-          </Button>
-        </li>
-        <li>
-          <Button btnType="light" type="button">
-            Sign Up
-          </Button>
-        </li>
-      </ul>
-      <input type="checkbox" />
-    </nav>
+    <NavWrapper>
+      <NavList>
+        <NavItem>
+          <NavLink href="/">Product</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/">Customers</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/">Pricing</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/">Resources</NavLink>
+        </NavItem>
+      </NavList>
+    </NavWrapper>
   );
 };
 
